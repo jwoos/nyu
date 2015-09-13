522e51be2cd17c314e9865b3c1f19c9299032917
Jun Woo Shin <shin.junwoo97@gmail.com>
2015-09-13 12:38:39 -0400

    1

--- lab/1/print.py
+++ lab/1/print.py
@@ -1,23 +1,8 @@
-"""
-name = input('What\'s your name? \n')
-print('Your name is %s' % name)
-"""
+print('My name is Junwoo Shin')
 
-"""
 first = int(input('Please put in the first number: '))
 second = int(input('Please put in the second number: '))
-
-
-def operations(first, second):
-	print('The sum is %s, the product is %s, and the difference is %s' % (first + second, first * second, first - second))
-
-operations(first, second)
-"""
+print('The sum is %s, the product is %s, and the difference is %s' % (first + second, first * second, first - second))
 
 temp = int(input('Please put in the temperature in Farenheit: '))
-
-
-def convert(temp):
-	print('The temperature in Celcius is: ' + str((temp - 32) * 5 / 9))
-
-convert(temp)
+print('The temperature in Celcius is: ' + str((temp - 32) * 5 / 9))
