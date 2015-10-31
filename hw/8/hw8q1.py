380efd04424f1c7fb15dd8d5df531b885a4230e0
Jun Woo Shin <shin.junwoo97@gmail.com>
2015-10-31 16:02:57 -0400

    wip

--- hw/8/hw8q1.py
+++ hw/8/hw8q1.py
@@ -4,8 +4,62 @@ Name: Junwoo Shin
 NetID: JS8460
 Filename: hw8q1.py
 
-Explanation:
+Explanation
+-----------
+First I printed the days at the top which would have been more concise looping over a tuple and printing.
+The optional parameter end was used to allow printing on same line with even space. 
+I first printed the spaces based on what day it started. After that I print the first line of numbers. If it's one digit I right align by checking if it's only one digit and adding a space at the front if it is.
+After the first line of numbers were printed I did repeated the same thing for the second and looped over in series of 7's. It would continue right until day_current was greater than day_count.
+
 
 '''
 
+def calendar(day_count, day_start):
+	day_current = 1
 
+	print('Mo', end='\t')
+	print('Tu', end='\t')
+	print('We', end='\t')
+	print('Th', end='\t')
+	print('Fr', end='\t')
+	print('Sa', end='\t')
+	print('Su')
+
+	for spaces in range(day_start - 1):
+		print('', end='\t')
+		day_current = spaces
+
+	for first_row in range(1, 7 - day_start + 2):
+		if len(str(first_row)) != 2:
+			print(' ' + str(first_row), end='\t')
+		elif day_current <= day_count:
+			print(first_row, end='\t')
+		day_current = first_row
+	day_current += 1
+	print('')
+
+	while day_current <= day_count:
+		for current_index in range(7):
+			if len(str(day_current)) != 2:
+				print(' ' + str(day_current), end='\t')
+			elif day_current < day_count:
+				print(day_current, end='\t')
+			elif day_current == day_count:
+				print(day_current)
+				return current_index + 1, current_index + 2
+			day_current += 1
+		print('')
+
+def leapyear(year):
+	if year % 4 == 0:
+		if year % 100 == 0 and year % 400 == 0:
+			return True
+		elif year % 100 == 0 and year % 400 != 0:
+			return False
+		else:
+			return True
+	else:
+		return False
+
+def main():
+	
---
+++ hw/8/hw8q2.py
@@ -0,0 +1,54 @@
+'''
+
+Name: Junwoo Shin
+NetID: JS8460
+Filename: hw8q2.py
+
+Explanation
+-----------
+In the first method in the occurrence of the first space it will stop adding letters to the word variable and return the variable. Spaces indicate the completion of a word.
+Using the same logic for the second function the index was used to get the first occurrence of a space. The index was then used to slice and return the string without the first word.
+To reverse it, I use two for loops where I iterate over two loops using the first one to decrease the second one. The second loop determines how many times the first word is removed and since it's getting smaller more of the original words remain towards the end. This allows the use of the first method to get the word and add it to final_string.
+When both loops finish it will return the reversed string
+
+'''
+
+
+def get_word(the_string):
+	word = ''
+	if ' ' in the_string:
+		for index in range(len(the_string)):
+			if the_string[index] != ' ':
+				word += the_string[index]
+			else:
+				return word
+	else:
+		return the_string
+
+
+def remove_word(the_string):
+	for index in range(len(the_string)):
+		if the_string[index] == ' ':
+			return the_string[index + 1::]
+
+
+def reverse_sentence(the_string):
+	space_count = 0
+	for index in range(len(the_string)):
+		if the_string[index] == ' ':
+			space_count += 1
+	word_count = space_count + 1
+
+	final_string = ''
+	for x in range(1, word_count + 1):
+		temp_string = the_string
+		for y in range(0, word_count - x):
+			temp_string = remove_word(temp_string)
+		final_string += get_word(temp_string) + ' '
+	return(final_string)
+
+
+def main():
+	user_input = input('Please enter a string to reverse: ')
+	user_input_reverse = reverse_sentence(user_input)
+	print('The reversed sentence is: \n %s' % user_input_reverse)
