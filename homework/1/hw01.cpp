510a656b0cb34e90ed2efbc861c161cd814ced45
Junwoo Shin <shin.junwoo97@gmail.com>
2016-02-01 16:26:43 -0500

    changed hw

--- homework/1/hw01.cpp
+++ homework/1/hw01.cpp
@@ -1,34 +1,15 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <vector>
 using namespace std;
 
 const char offset = 'a';
 const int alphabetLength = 26;
 
-char decryptChar(int shift, char encryptedChar) {
-	// check if upper, space or punctuation
-	if (isupper(encryptedChar) || isspace(encryptedChar) || ispunct(encryptedChar)) {
-		return encryptedChar;
-	} else {
-		char temp = encryptedChar - offset - shift;
-		// if it ends up negative, add the length of the alphabet to it to bring it around at the end
-		if (temp < 0) {
-			return temp + offset + alphabetLength;
-		} else {
-			return temp + offset;
-		}
-	}
-}
-
-void decryptString(int shift, string& encryptedString) {
-	for (size_t index = 0; index < encryptedString.size(); index++) {
-		// decrypt each character in a string and change the string in place
-		encryptedString[index] = decryptChar(shift, encryptedString[index]);
-	}
-}
+char decryptChar(int shift, char encryptedChar);
+void decryptString(int shift, string& encryptedString);
 
 int main() {
 	// open file
@@ -59,5 +40,27 @@ int main() {
 	// I used int for index rather than size_t as the latter is unsigned and would never become negative
 	for (int index = decryptedVector.size() - 1; index >= 0; index--) {
 		cout << decryptedVector[index] << endl;
+	}
+}
+
+char decryptChar(int shift, char encryptedChar) {
+	// check if upper, space or punctuation
+	if (isupper(encryptedChar) || isspace(encryptedChar) || ispunct(encryptedChar)) {
+		return encryptedChar;
+	} else {
+		char temp = encryptedChar - offset - shift;
+		// if it ends up negative, add the length of the alphabet to it to bring it around at the end
+		if (temp < 0) {
+			return temp + offset + alphabetLength;
+		} else {
+			return temp + offset;
+		}
+	}
+}
+
+void decryptString(int shift, string& encryptedString) {
+	for (size_t index = 0; index < encryptedString.size(); index++) {
+		// decrypt each character in a string and change the string in place
+		encryptedString[index] = decryptChar(shift, encryptedString[index]);
 	}
 }
