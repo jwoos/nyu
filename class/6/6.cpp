bf589e6473e145de182e5ef73b1b3a263b44c81a
Jun Woo Shin <jwoos@users.noreply.github.com>
2016-02-10 16:25:42 -0500

    Create 6.cpp

---
+++ class/6/6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+class Date {
+    public:
+        Date(int month, int day, int year) :month(month), day(day), year(year) {}
+    
+        void display() const {
+            cout << month << '/' << day << '/' << year << endl;
+        }
+    private:
+        int month, day, year;
+};
+
+class Person {
+    public:
+        Person(const string& theName) :name(theName) {}
+    private:
+    
+}
+
+int main() {
+    Date date(10, 1, 1997);
+}
