8b1d62e9fef460c1b778f568e8145e98186a6616
Junwoo Shin <shin.junwoo97@gmail.com>
2016-03-19 12:20:13 -0400

    hw06 done

--- homework/6/hw06.cpp
+++ homework/6/hw06.cpp
@@ -6,27 +6,89 @@
 
 using namespace std;
 
+// a person has a name and can be alive or dead
+// universal methods and attributes
 class Person {
 	public:
 		Person(const string& name) : name(name), alive(true) {}
 
+		virtual float getStrength() const = 0;
+
+		virtual void setStrength(float newStrength) = 0;
+
 		string getName() const {
 			return name;
+		}
+
+		bool live() const {
+			return alive;
+		}
+
+		void die() {
+			alive = false;
 		}
 	private:
 		string name;
 		bool alive;
 };
 
+// nobles inherit from person
 class Noble : public Person {
 	public:
 		Noble(const string& name) : Person(name) {}
 
 		virtual void battle(Noble& enemy) = 0;
 
-		virtual float getStrength() const = 0;
+		// actual battle method was split up to allow reuse in derived classes
+		void declareBattle(Noble*& enemy) {
+			cout << this -> getName() << " battles " << enemy -> getName() << endl;
+		}
 
-		virtual void setStrength(float newStrength) = 0;
+		// battling and deciding strength/death
+		virtual void engageInBattle(Noble*& enemy) {
+			string selfName = this -> getName();
+			string enemyName = enemy -> getName();
+
+			float selfTotalStrength = getStrength();
+			float enemyTotalStrength = enemy -> getStrength();
+
+			bool selfAlive = this -> live();
+			bool enemyAlive = enemy -> live();
+
+			if (enemyTotalStrength == selfTotalStrength) {
+				if (!enemyAlive && !selfAlive) {
+					cout << "Oh NO! They're both dead! Yuck!" << endl;
+				} else {
+					// since both Lords and PersonWithStrengthToFight have setStrength() and getStrength() that does their own thing I don't have to worry about the difference in their sources of strength or how differences are calculated here
+					setStrength(enemyTotalStrength);
+					enemy -> setStrength(selfTotalStrength);
+
+					cout << "Mutual annihilation: " << selfName << " and " << enemyName << " die at each other's hands" << endl;
+				}
+			} else if (selfTotalStrength > enemyTotalStrength) {
+				if (!enemyAlive) {
+					cout << "He's dead, " << selfName << endl;
+				} else {
+					setStrength(enemyTotalStrength);
+					enemy -> setStrength(selfTotalStrength);
+					enemy -> die();
+
+					cout << selfName << " defeats " << enemyName << endl;
+				}
+			} else if (selfTotalStrength < enemyTotalStrength) {
+				if (!selfAlive) {
+					cout << "He's dead, " << enemyName << endl;
+				} else {
+					setStrength(enemyTotalStrength);
+					enemy -> setStrength(selfTotalStrength);
+					this -> die();
+
+					cout << enemyName << " defeats " << selfName << endl;
+				}
+			}
+
+			cout << endl;
+		}
 	private:
 };
 
@@ -44,9 +106,13 @@ class Protector : public Person {
 			boss = &aNoble;
 		}
 
-		virtual float getStrength() const = 0;
+		float getStrength() const {
+			return strength;
+		};
 
-		virtual void setStrength(float newStrength) = 0;
+		void setStrength(float newStrength) {
+			strength = newStrength;
+		};
 	private:
 		float strength;
 		Noble* boss;
@@ -56,15 +122,25 @@ class PersonWithStrengthToFight : public Noble {
 	public:
 		PersonWithStrengthToFight(const string& name, float strength) : Noble(name), strength(strength) {}
 
-		void setStrength(float newStrength) {
-			strength = newStrength;
+		void setStrength(float enemyStrength) {
+			if (enemyStrength > strength || enemyStrength == strength) {
+				strength = 0;
+			} else if (enemyStrength < strength) {
+				strength -= enemyStrength;
+			}
 		}
 
 		float getStrength() const {
 			return strength;
 		}
 
+		// making use of parent methods
 		void battle(Noble& enemy) {
+			Noble* enemyP = &enemy;
+
+			// normal battle
+			declareBattle(enemyP);
+			engageInBattle(enemyP);
 		}
 	private:
 		float strength;
@@ -74,58 +150,7 @@ class Lord : public Noble {
 	public:
 		Lord(const string& name) : Noble(name) {}
 
-		void battle(Noble*& enemy) {
-			float selfTotalStrength;
-			float enemyTotalStrength;
-			float ratio;
-
-			// get strength of the instance
-			selfTotalStrength = getStrength();
-			// strength of the enemy's instance
-			enemyTotalStrength = enemy -> getStrength();
-
-			cout << this -> getName() << " battles " << enemy -> getName() << endl;
-
-			if (enemyTotalStrength == selfTotalStrength) {
-				if (enemyTotalStrength == 0 && selfTotalStrength == 0) {
-					cout << "Oh NO! They're both dead! Yuck!" << endl;
-				} else {
-					// ratio is set so that the loser is on the top always
-					ratio = selfTotalStrength / enemyTotalStrength;
-					setStrength(ratio);
-					enemy -> setStrength(ratio);
-
-					cout << "Mutual annihilation: " << this -> getName() << " and " << enemy -> getName() << " die at each other's hands" << endl;
-				}
-			} else if (selfTotalStrength > enemyTotalStrength) {
-				if (enemyTotalStrength == 0) {
-					cout << "He's dead, " << this -> getName() << endl;
-				} else {
-					ratio = enemyTotalStrength / selfTotalStrength;
-
-					setStrength(ratio);
-					enemy -> setStrength(1);
-
-					cout << this -> getName() << " defeats " << enemy -> getName() << endl;
-				}
-			} else if (selfTotalStrength < enemyTotalStrength) {
-				if (selfTotalStrength == 0) {
-					cout << "He's dead, " << enemy -> getName() << endl;
-				} else {
-					ratio = selfTotalStrength / enemyTotalStrength;
-
-					setStrength(1);
-					enemy -> setStrength(ratio);
-
-					cout << enemy -> getName() << " defeats " << this -> getName() << endl;
-				}
-			}
-
-			cout << endl;
-		}
-
 		float getStrength() const {
-
 			float totalStrength = 0;
 
 			for (Protector* warriorPofloater : army) {
@@ -136,10 +161,81 @@ class Lord : public Noble {
 		}
 
 		// sets the strength of each warrior based on the ratio
-		void setStrength(float ratio) {
-			for (Protector*& warriorPofloater : army) {
-				float oldStrength = warriorPofloater -> getStrength();
-				warriorPofloater -> setStrength(oldStrength - (oldStrength * ratio));
+		void setStrength(float enemyStrength) {
+			float ratio;
+
+			if (enemyStrength > getStrength() || enemyStrength == getStrength()) {
+				ratio = 1;
+			} else if (enemyStrength < getStrength()) {
+				ratio = enemyStrength / getStrength();
+			}
+
+			for (Protector*& warriorPointer : army) {
+				float oldStrength = warriorPointer -> getStrength();
+				warriorPointer -> setStrength(oldStrength - (oldStrength * ratio));
+			}
+		}
+
+		void hires(Protector& aProtector) {
+			if (aProtector.getBoss() == nullptr && this -> live() && aProtector.live()) {
+				aProtector.setBoss(*this);
+				army.push_back(&aProtector);
+			}
+		}
+
+		void fires(Protector& aProtector) {
+			size_t protectorIndex;
+
+			for (size_t index = 0; index < army.size(); index++) {
+				if (army[index] -> getName() == aProtector.getName()) {
+					protectorIndex = index;
+					break;
+				}
+			}
+
+			cout << aProtector.getName() << " , you're fired -- " << this -> getName() << endl;
+
+			army[protectorIndex] = army[army.size() - 1];
+			army.pop_back();
+		}
+
+		void battle(Noble& enemy) {
+			// battle but it also declares battle for each protector in between
+			Noble* enemyP = &enemy;
+			declareBattle(enemyP);
+
+			if (this -> live()) {
+				for (size_t i = 0; i < army.size(); i++) {
+					if (army[i] -> live()) {
+						army[i] -> declareBattle();
+					}
+				}
+			}
+
+			engageInBattle(enemyP);
+		}
+
+		void engageInBattle(Noble*& enemy) {
+			Noble::engageInBattle(enemy);
+
+			bool selfAlive = this -> live();
+			bool enemyAlive = enemy -> live();
+
+			float selfTotalStrength = getStrength();
+			float enemyTotalStrength = enemy -> getStrength();
+
+			if (enemyTotalStrength == selfTotalStrength) {
+				if (enemyAlive && selfAlive) {
+					for (size_t i = 0; i < army.size(); i++) {
+						army[i] -> die();
+					}
+				}
+			} else if (selfTotalStrength < enemyTotalStrength) {
+				if (selfAlive) {
+					for (size_t i = 0; i < army.size(); i++) {
+						army[i] -> die();
+					}
+				}
 			}
 		}
 	private:
@@ -151,7 +247,7 @@ class Wizard : public Protector {
 		Wizard(const string& name, float strength) : Protector(name, strength) {}
 
 		void declareBattle() const {
-			cout << "POOF!";
+			cout << "POOF!" << endl;
 		}
 	private:
 };
@@ -170,9 +266,9 @@ class Archer : public Warrior {
 	public:
 		Archer(const string& name, float strength) : Warrior(name, strength) {}
 
-		void declarebattle() const {
+		void declareBattle() const {
 			cout << "TWANG!";
-			Protector::declareBattle();
+			Warrior::declareBattle();
 		}
 	private:
 };
@@ -181,36 +277,36 @@ class Swordsman : public Warrior {
 	public:
 		Swordsman(const string& name, float strength) : Warrior(name, strength) {}
 
-		void declarebattle() const {
+		void declareBattle() const {
 			cout << "CLANG!";
-			Protector::declareBattle();
+			Warrior::declareBattle();
 		}
 	private:
 };
 
 int main() {
-	//Lord sam("Sam");
-	//Archer samantha("Samantha", 200);
-	//sam.hires(samantha);
-	//Lord joe("Joe");
-	//PersonWithStrengthToFight randy("Randolf the Elder", 250);
-	//joe.battle(randy);
-	//joe.battle(sam);
-	//Lord janet("Janet");
-	//Swordsman hardy("TuckTuckTheHardy", 100);
-	//Swordsman stout("TuckTuckTheStout", 80);
-	//janet.hires(hardy);
-	//janet.hires(stout);
-	//PersonWithStrengthToFight barclay("Barclay the Bold", 300);
-	//janet.battle(barclay);
-	//janet.hires(samantha);
-	//Archer pethora("Pethora", 50);
-	//Archer thora("Thorapleth", 60);
-	//Wizard merlin("Merlin", 150);
-	//janet.hires(pethora);
-	//janet.hires(thora);
-	//sam.hires(merlin);
-	//janet.battle(barclay);
-	//sam.battle(barclay);
-	//joe.battle(barclay);
+	Lord sam("Sam");
+	Archer samantha("Samantha", 200);
+	sam.hires(samantha);
+	Lord joe("Joe");
+	PersonWithStrengthToFight randy("Randolf the Elder", 250);
+	joe.battle(randy);
+	joe.battle(sam);
+	Lord janet("Janet");
+	Swordsman hardy("TuckTuckTheHardy", 100);
+	Swordsman stout("TuckTuckTheStout", 80);
+	janet.hires(hardy);
+	janet.hires(stout);
+	PersonWithStrengthToFight barclay("Barclay the Bold", 300);
+	janet.battle(barclay);
+	janet.hires(samantha);
+	Archer pethora("Pethora", 50);
+	Archer thora("Thorapleth", 60);
+	Wizard merlin("Merlin", 150);
+	janet.hires(pethora);
+	janet.hires(thora);
+	sam.hires(merlin);
+	janet.battle(barclay);
+	sam.battle(barclay);
+	joe.battle(barclay);
 }
