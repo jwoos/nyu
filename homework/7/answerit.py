386db1e6e14f9af206e540997cce7aabd8aa6275
Junwoo Shin <shin.junwoo97@gmail.com>
2016-03-19 13:26:18 -0400

    hw 7

--- homework/7/answerit.py
+++ homework/7/answerit.py
@@ -3,28 +3,55 @@ import re
 
 connection = socket.create_connection(('gnarlygno.me', 8888))
 
+RETURN = '\r'
+
+def convert_dec_bin(dec_num):
+    return bin(dec_num)
+
+def convert_dec_hex(dec_num):
+    return hex(dec_num)
+
 while connection:
-    print('beginning')
     data_received = ''
     data_byte = connection.recv(4096)
     if not data_byte:
         break
     data_str = data_byte.decode('utf-8')
-    print(data_str)
+    print(data_str.strip())
 
-    if re.match(r'^\r\n\r\n', data_str):
+    if re.match(r'(?:[a-zA-Z \d]+){0,1}\r\n\r\n', data_str):
         data_received = data_str.split('\r\n')[-2]
     else:
         data_received = data_str
 
     data_received = data_received.strip()
-    matched = re.match(r'Convert \((\d+)\) from base (\d+) to base \((\d+)\)', data_received)
+    matched_convert = re.match(r'Convert \((\d+)\) from base (\d+) to base \((\d+)\)', data_received)
 
-    if matched:
-        print(matched.group(1))
-        print(matched.group(2))
-        print(matched.group(3))
-        data_to_send = b'100'
+    if matched_convert:
+        number = int(matched_convert.group(1))
+        base_from = int(matched_convert.group(2))
+        base_to = int(matched_convert.group(3))
+        data_to_send = ''
+
+        if base_from == 10:
+            if base_to == 2:
+                data_to_send += convert_dec_bin(number)
+            elif base_to == 16:
+                data_to_send += convert_dec_hex(number)
+            elif base_to == 10:
+                data_to_send += str(number)
+            print(data_to_send)
+            data_to_send += RETURN
+            data_to_send = data_to_send.encode()
+
         connection.sendall(data_to_send)
-        print('sent')
-    print('end')
+
+    matched_name = re.match(r'How to spell head TA name\?{3}', data_received)
+
+    if matched_name:
+        data_to_send = 'Salsaman'
+        print(data_to_send)
+        data_to_send = (data_to_send + RETURN).encode()
+        connection.sendall(data_to_send)
+
+# video = https://youtu.be/mAbTzPoJgWs
