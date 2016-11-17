7b9824369c4144ab677a6fb395e561a584ce396a
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-11-17 10:36:00 -0500

    class 21

---
+++ class/21/README.md
@@ -0,0 +1,8 @@
+# Lecture 21
+
+Removal
+- if a node is red, just remove it
+
+
+### b-tree
+nodes in a b-tree have m pointers
---
+++ class/21/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
