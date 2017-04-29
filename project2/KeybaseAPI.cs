1fb9b5053f099faac287571b09c646189a473cee
Unknown <pi.lanningham@gmail.com>
2017-04-29 17:56:03 -0400

    Looks up by username too
    This is a bit of a hack... Instead, we could be using their "autocomplete" endpoint, but I had implement all the discover stuff before I realized it existed.

--- project2/KeybaseAPI.cs
+++ project2/KeybaseAPI.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using RestSharp;
 
@@ -20,13 +21,6 @@ namespace Keybase
                 public int algo { get; set; }
             }
             public class RemoteProofs {
-                public class DNSProof
-                {
-                    public string domain { get; set; }
-                    public string protocol { get; set; }
-                    public string searchable { get; set; }
-                }
-                public List<DNSProof> dns { get; set; }
                 public string twitter { get; set; }
                 public string github { get; set; }
                 public string reddit { get; set; }
@@ -38,7 +32,6 @@ namespace Keybase
             public string uid { get; set; }
             public Key public_key { get; set; }
             public string full_name { get; set; }
-            public long ctime { get; set; }
             public RemoteProofs remote_proofs { get; set; }
 
             public override string ToString() {
@@ -59,17 +52,32 @@ namespace Keybase
                 public string username { get; set; }
                 public string salt { get; set; }
             }
+            public class Profile {
+                public string full_name { get; set; }
+            }
             public class KeyEntry {
                 public class PrimaryKey {
                     public string kid { get; set; }
                     public string key_type { get; set; }
                     public string bundle { get; set; }
+                    public string key_fingerprint { get; set; }
+                    public int key_bits { get; set; }
+                    public int key_algo { get; set; }
                 }
                 public PrimaryKey primary { get; set; }
             }
+            public class RemoteProofs {
+                public class Proof {
+                    public string proof_type { get; set; }
+                    public string nametag { get; set; }
+                }
+                public List<Proof> all { get; set; }
+            }
             public string id { get; set; }
             public Basics basics { get; set; }
+            public Profile profile { get; set; }
             public KeyEntry public_keys { get; set; }
+            public RemoteProofs proofs_summary { get; set; }
 
         }
         public Status status { get; set; }
@@ -86,14 +94,43 @@ namespace Keybase
             foreach(var source in sources) {
                 request.AddParameter(source, name);
             }
-            return client.Get<DiscoverResponse>(request).Data;
+            var discovered = client.Get<DiscoverResponse>(request).Data;
+
+            // HACK: make a separate request for the user itself, just in case
+            var lookedUp = Lookup(name);
+            if(lookedUp.status.code == 0 && lookedUp.them.Count > 0) {
+                var them = lookedUp.them.First();
+                if(!discovered.matches.Any(x => x.uid == them.id)) {
+                    var spoofedDiscover = new DiscoverResponse.Match {
+                        username = name,
+                        uid = them.id,
+                        public_key = new DiscoverResponse.Match.Key {
+                            key_fingerprint = them.public_keys.primary.key_fingerprint,
+                            bits = them.public_keys.primary.key_bits,
+                            algo = them.public_keys.primary.key_algo
+                        },
+                        full_name = them.profile.full_name,
+                        remote_proofs = new DiscoverResponse.Match.RemoteProofs {
+                            twitter = them.proofs_summary.all.FirstOrDefault(x => x.proof_type == "twitter")?.nametag,
+                            reddit = them.proofs_summary.all.FirstOrDefault(x => x.proof_type == "reddit")?.nametag,
+                            facebook = them.proofs_summary.all.FirstOrDefault(x => x.proof_type == "facebook")?.nametag,
+                            github = them.proofs_summary.all.FirstOrDefault(x => x.proof_type == "github")?.nametag,
+                            hackernews = them.proofs_summary.all.FirstOrDefault(x => x.proof_type == "hackernews")?.nametag,
+                            coinbase = them.proofs_summary.all.FirstOrDefault(x => x.proof_type == "coinbase")?.nametag
+                        }
+                    };
+                    discovered.matches.Add(spoofedDiscover);
+                }
+            }
+            
+            return discovered;
         }
         public static LookupResponse Lookup(string username)
         {
             var client = new RestClient("https://keybase.io/_/api/1.0/");
             var request = new RestRequest("user/lookup.json");
             request.AddParameter("usernames", username);
-            request.AddParameter("fields", "basics,public_keys");
+            request.AddParameter("fields", "basics,public_keys,proofs_summary,profile");
             foreach(var source in sources) {
                 request.AddParameter(source, username);
             }
