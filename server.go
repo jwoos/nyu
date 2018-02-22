2e64223a446a1635010994a5330a7fca2c2466be
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-02-22 15:33:29 -0500

    base

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+func newRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/status", status).Methods("GET")
+	return router
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
