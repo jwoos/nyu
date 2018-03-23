70757208156b6c7887c6cc0d02b9a2981229fdb2
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-03-23 19:56:35 +0000

    comments

--- ai.go
+++ ai.go
@@ -57,27 +57,35 @@ func evaluation(state *checkers.StateByte, who byte) float64 {
 			danger[move.Jump] = true
 		}
 
+		// how many are safe based on how many are in danger
 		evaluation += float64(len(state.White) - len(danger))
 	} else {
+		// ratio of own piece to oppponent pieces
 		evaluation = -float64(len(state.Black)) / float64(len(state.White))
 
+		// capture moves
 		captureMoves := len(state.PossibleCaptureMovesAll(MIN))
 		evaluation -= 1.5 * float64(captureMoves)
 
+		// how many possible moves
 		evaluation -= 0.5 * float64(len(state.PossibleMovesAll(MIN)) - captureMoves)
 
+		// how many pieces are in danger
 		danger := make(map[checkers.Coordinate]bool)
 		for move, _ := range state.PossibleCaptureMovesAll(MAX) {
 			danger[move.Jump] = true
 		}
 
+		// how many are safe based on how many are in danger
 		evaluation -= float64(len(state.Black) - len(danger))
 	}
 
 	return evaluation
 }
 
+// minimax driver
 func minimaxAB(state *checkers.StateByte, depth int) (checkers.Move, Stat) {
+	// stat structure to keep track of information about the search
 	stat := Stat{}
 
 	lln("STARTING MINIMAX")
@@ -87,17 +95,22 @@ func minimaxAB(state *checkers.StateByte, depth int) (checkers.Move, Stat) {
 	return move, stat
 }
 
+// max player's move
 func max(state *checkers.StateByte, alpha float64, beta float64, depth int, stat *Stat) Node {
+	// check if current depth is higher than previously set max
 	if stat.Start - depth > stat.Levels {
 		stat.Levels = stat.Start - depth
 	}
+	// incremenet for node generated
 	stat.Generated++
 	lln("MAX")
 
 	node := Node{}
 	node.State = state
+	// v value
 	node.Heuristic = math.Inf(-1)
 
+	// check if game is over
 	w, s := state.GameEnd()
 	if w {
 		if s == MAX {
@@ -109,24 +122,29 @@ func max(state *checkers.StateByte, alpha float64, beta float64, depth int, stat
 		return node
 	}
 
+	// reached the end but game isn't over so we want to use an evaluation function
 	if depth == 0 {
 		node.Heuristic = evaluation(node.State, MAX)
 		lln("Heuristic:", node.Heuristic)
 		return node
 	}
 
+	// branch from all possible moves
 	moves := state.PossibleMovesAll(state.Turn)
 	lln(moves)
 	for move, _ := range moves {
 		newState := state.Copy()
 		newState.Move(move)
 
+		// branch deeper
 		v := math.Max(node.Heuristic, min(newState, alpha, beta, depth-1, stat).Heuristic)
+		// if the current value is different from the heuristic, change the move
 		if v != node.Heuristic {
 			node.Move = move
 		}
 		node.Heuristic = v
 
+		// v >= beta should it be pruned?
 		if node.Heuristic >= beta {
 			stat.MaxPruned++
 			return node
@@ -138,6 +156,7 @@ func max(state *checkers.StateByte, alpha float64, beta float64, depth int, stat
 	return node
 }
 
+// min player's move
 func min(state *checkers.StateByte, alpha float64, beta float64, depth int, stat *Stat) Node {
 	if stat.Start - depth > stat.Levels {
 		stat.Levels = stat.Start - depth
@@ -160,24 +179,30 @@ func min(state *checkers.StateByte, alpha float64, beta float64, depth int, stat
 		return node
 	}
 
+	// reached the end but game isn't over so we want to use an evaluation function
 	if depth == 0 {
 		node.Heuristic = evaluation(node.State, MIN)
 		lln("Heuristic:", node.Heuristic)
 		return node
 	}
 
+	// branch from all possible moves
 	moves := state.PossibleMovesAll(state.Turn)
 	lln(moves)
 	for move, _ := range moves {
 		newState := state.Copy()
 		newState.Move(move)
 
+		// branch deeper
 		v := math.Min(node.Heuristic, max(newState, alpha, beta, depth-1, stat).Heuristic)
+		// if the current value is different from the heuristic, change the move
 		if v != node.Heuristic {
 			node.Move = move
 		}
 		node.Heuristic = v
 
+		// v <= alpha - should it be pruned?
+		if node.Heuristic >= beta {
 		if node.Heuristic <= alpha {
 			stat.MinPruned++
 			return node
