829fb7a86d0f95c54d9ff0f467e6721653db6263
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-03 17:35:12 -0400

    some cleanup and grammar

---
+++ project/2/parser/parser.py
@@ -0,0 +1,201 @@
+from ply import yacc
+
+from scanner import scanner
+
+tokens = scanner.tokens
+
+precedence = (
+    ('right', 'uminus'),
+    ('left', 'MULTIPLY', 'DIVIDE'),
+    ('left', 'PLUS', 'MINUS'),
+    ('right', 'ASSIGN'),
+)
+
+def p_program(p):
+    '''
+    program : function_def program
+            | decl program
+            | function_decl program
+            | empty
+    '''
+    pass
+
+def p_decl(p):
+    '''
+    decl : kind var_list SEMI
+    '''
+    pass
+
+def p_kind(p):
+    '''
+    kind : int_kw
+         | float_kw
+    '''
+    pass
+
+def p_var_list(p):
+    '''
+    var_list : identifier var_list_prime
+    '''
+    pass
+
+def p_var_list_prime(p):
+    '''
+    var_list_prime : COMMA identifier var_list
+                   | empty
+    '''
+    pass
+
+def p_func_decl(p):
+    '''
+    func_decl : kind identifier LPAR kind RPAR SEMI
+    '''
+    pass
+
+def p_func_def(p):
+    '''
+    func_def : kind identifier LPAR kind identifier RPAR body
+    '''
+    pass
+
+def p_body(p):
+    '''
+    body : LBRACE body_prime RBRACE
+    '''
+    pass
+
+def p_body_prime(p):
+    '''
+    body_prime : decl body_prime
+               | stmt body_prime
+               | empty
+    '''
+    pass
+
+def p_stmt(p):
+    '''
+    stmt : expr SEMI
+         | if_kw LPAR expr RPAR stmt else_stmt
+         | while_kw LPAR bool_expr RPAR stmt
+         | read_kw var_list SEMI
+         | write_kw expr_list SEMI
+         | return_kw expr SEMI
+    '''
+    pass
+
+def p_expr_list(p):
+    '''
+    expr_list : expr expr_list_prime
+    '''
+    pass
+
+def p_expr_list_prime(p):
+    '''
+    expr_list_prime : COMMA expr
+                    | empty
+    '''
+    pass
+
+def p_else_stmt(p):
+    '''
+    else_stmt : else_kw stmt
+              | empty
+    '''
+    pass
+
+def p_factor(p):
+    '''
+    factor : identifier
+           | integer_literal
+           | float_literal
+           | function_call
+           | LPAR expr RPAR
+    '''
+    pass
+
+def p_bool_expr(p):
+    '''
+    bool_expr : expr bool_op expr
+    '''
+    pass
+
+def p_function_call(p):
+    '''
+    function_call : identifier LPAR expr RPAR
+    '''
+    pass
+
+def p_term(p):
+    '''
+    term : uminus term_prime
+    '''
+    pass
+
+def p_term_prime(p):
+    '''
+    term_prime : mulop uminus term_prime
+               | empty
+    '''
+    pass
+
+def p_uminus(p):
+    '''
+    uminus : MINUS factor %prec uminus
+           | factor
+    '''
+    pass
+
+def p_mulop(p):
+    '''
+    mulop : MULTIPLY
+          | DIVIDE
+    '''
+    pass
+
+def p_expr1(p):
+    '''
+    expr1 : term expr1_prime
+    '''
+    pass
+
+def p_expr1_prime(p):
+    '''
+    expr1_prime : add_op term expr1_prime
+                | empty
+    '''
+    pass
+
+def p_add_op(p):
+    '''
+    add_op : PLUS
+           | MINUS
+    '''
+    pass
+
+def p_bool_op(p):
+    '''
+    bool_op : LT
+            | GT
+            | EQUAL
+            | GE
+            | LT
+    '''
+    pass
+
+def p_expr(p):
+    '''
+    expr : identifier ASSIGN expr SEMI
+         | expr1 SEMI
+    '''
+    pass
+
+def p_empty(p):
+    '''
+    empty :
+    '''
+    pass
+
+def p_error(p):
+    print(p)
+
+parser = yacc.yacc()
---
+++ project/2/scanner/driver.py
@@ -0,0 +1,24 @@
+import scanner
+
+# Give the lexer some input
+with open('TEST_EXHAUSTIVE', 'r') as inf:
+    tok1.lexer.input(inf.read(-1))
+# lexer.input(data)
+
+with open('TEST_EXHAUSTIVE_OUTPUT','w') as f:
+    f.write("token\t" +  "token\t" + "token\n")
+    f.write("number\t" + "name\t"   + "value\n")
+# Tokenize
+
+print("token\t" +  "token\t" + "token")
+
+while True:
+    tok = tok1.lexer.token()
+    if not tok:
+        print("FOUND EOF")
+        break      # No more input
+    tok.lexpos = tok1.token_dictionary[tok.type]
+    with open('TEST_EXHAUSTIVE_OUTPUT','a') as f:
+        f.write(str(tok.lexpos) + '\t\t' + str(tok.type) + '\t\t' + '"'+str(tok.value)+'"' + '\n')
+
+    print(str(tok.lexpos) + '\t' + str(tok.type) + '\t' + '"'+str(tok.value)+'"')
