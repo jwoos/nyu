ea2b619b71734943b8e671f063ab0f02fc00cc1d
YinWang3026 <yw2178@nyu.edu>
2018-04-10 00:22:24 -0400

    indent

--- src/views/login.py
+++ src/views/login.py
@@ -6,10 +6,10 @@ from src.db import connection
 class LoginView(MethodView):
     def get(self):
         body = request.get_json();
-            with connection.cursor() as cursor:
-        cursor.callproc('login_user',body['email'],body['pw']);
-        data = cursor.fetchall();
-        if len(data) == 1:
-            return jsonify({'status': 'okay'}), 200
-        else
-            return jsonify({'status': 'not okay'}), 200
+        with connection.cursor() as cursor:
+            cursor.callproc('login_user',body['email'],body['pw']);
+            data = cursor.fetchall();
+            if len(data) == 1:
+                return jsonify({'status': 'okay'}), 200
+            else
+                return jsonify({'status': 'not okay'}), 200
