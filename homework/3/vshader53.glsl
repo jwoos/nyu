1e62ece9714b5be9c3c921fbb10623a1202d5dab
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-12 14:39:19 -0400

    hw3 base

---
+++ homework/3/vshader53.glsl
@@ -0,0 +1,58 @@
+/*
+File Name: "vshader53.glsl":
+Vertex shader:
+  - Per vertex shading for a single point light source;
+    distance attenuation is Yet To Be Completed.
+  - Entire shading computation is done in the Eye Frame.
+*/
+
+#version 150
+
+in  vec4 vPosition;
+in  vec3 vNormal;
+out vec4 color;
+
+uniform vec4 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform mat4 ModelView;
+uniform mat4 Projection;
+uniform mat3 Normal_Matrix;
+uniform vec4 LightPosition;   // Must be in Eye Frame
+uniform float Shininess;
+
+uniform float ConstAtt;  // Constant Attenuation
+uniform float LinearAtt; // Linear Attenuation
+uniform float QuadAtt;   // Quadratic Attenuation
+
+void main() {
+	// Transform vertex  position into eye coordinates
+	vec3 pos = (ModelView * vPosition).xyz;
+
+	vec3 L = normalize(LightPosition.xyz - pos);
+	vec3 E = normalize(-pos);
+	vec3 H = normalize(L + E);
+
+	// Transform vertex normal into eye coordinates
+	// vec3 N = normalize( ModelView*vec4(vNormal, 0.0) ).xyz;
+	vec3 N = normalize(Normal_Matrix * vNormal);
+
+	/*--- To Do: Compute attenuation ---*/
+	float attenuation = 1.0;
+
+	// Compute terms in the illumination equation
+	vec4 ambient = AmbientProduct;
+
+	float d = max(dot(L, N), 0.0);
+	vec4  diffuse = d * DiffuseProduct;
+
+	float s = pow(max(dot(N, H), 0.0), Shininess);
+	vec4 specular = s * SpecularProduct;
+
+	if (dot(L, N) < 0.0) {
+		specular = vec4(0.0, 0.0, 0.0, 1.0);
+	}
+
+	gl_Position = Projection * ModelView * vPosition;
+
+	/*--- attenuation below must be computed properly ---*/
+	color = attenuation * (ambient + diffuse + specular);
+}
