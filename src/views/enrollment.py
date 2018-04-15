21e6759d9250abfd88ce7874f64921f0bf93600c
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-15 05:02:33 +0000

    some more views

---
+++ src/views/enrollment.py
@@ -0,0 +1,27 @@
+from flask import request
+from flask.json import jsonify
+from flask.views import MethodView
+
+from src.db import connection
+
+
+class EnrollmentView(MethodView):
+    def get(self, enrollment_id=None):
+        with connection.cursor() as cursor:
+            if enrollment_id is None:
+                # all
+                cursor.execute('SELECT * FROM enrollments')
+            else:
+                # singular
+                cursor.execute('SELECT * FROM enrollments WHERE id=%(id)s', {'id': enrollment_id})
+
+            return jsonify(cursor.fetchall()), 200
+
+    def post(self):
+        raise NotImplementedError()
+
+    def patch(self):
+        raise NotImplementedError()
+
+    def delete(self):
+        raise NotImplementedError()
---
+++ src/views/professor.py
@@ -0,0 +1,35 @@
+from flask import request
+from flask.json import jsonify
+from flask.views import MethodView
+
+from src.db import connection
+
+
+class ProfessorView(MethodView):
+    def get(self, professor_id=None):
+        with connection.cursor() as cursor:
+            if professor_id is None:
+                # all
+                cursor.execute('SELECT * FROM professors')
+                return jsonify(cursor.fetchall()), 200
+            else:
+                # singular
+                cursor.execute('SELECT * FROM professors WHERE id=%(id)s', {'id': professor_id})
+                return jsonify(cursor.fetchone()), 200
+
+
+class ProfessorCourseView(MethodView):
+    def get(self, professor_id):
+        with connection.cursor() as cursor:
+            # all
+            cursor.execute(
+                'SELECT * FROM courses WHERE professor_id=%(professor_id)s',
+                {'professor_id': professor_id}
+            )
+
+            return jsonify(cursor.fetchall()), 200
+
+
+class ProfessorEvaluationView(MethodView):
+    def get(self, professor_id):
+        raise NotImplementedError()
