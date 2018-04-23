491c5b833030abd9aa8bdad0a65147fadda23d64
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-23 00:16:28 +0000

    add corresponding professors and students

--- src/errors.py
+++ src/errors.py
@@ -1,6 +1,7 @@
 DATA_SAVE = 'Error saving data'
 DATA_EMPTY = 'Data cannot be empty'
 FIELD_EMPTY = '{} cannot be empty'
+FILED_INVALID = '{} has an invalid value'
 AUTHENTICATION_WRONG = 'Email or password was wrong'
 AUTHENTICATION_INVALID = 'Invalid authentication'
 
--- src/views/account.py
+++ src/views/account.py
@@ -13,37 +13,57 @@ from src import auth, password, errors
 logger = logging.getLogger(__name__)
 
 
-# TODO create corresponding student/professor account
 class AccountView(MethodView):
     def post(self):
         body = request.get_json()
 
         if not body:
-            return jsonify({'error': DATA_EMPTY}), 403
+            return jsonify({'error': errors.DATA_EMPTY}), 422
 
         for k in ('email', 'password', 'class'):
             if not body.get(k):
-                return jsonify({'error': FIELD_EMPTY.format(k)})
+                return jsonify({'error': errors.FIELD_EMPTY.format(k)}), 422
 
-        body['password'] = generate(body['password'])
+        body['password'] = password.generate(body['password'])
+
+        if body['class'] == 'student':
+            for k in ('university_number',):
+                if not body.get(k):
+                    return jsonify({'error': errors.FIELD_EMPTY.format(k)}), 422
+        elif body['class'] == 'professor':
+            for k in ('first_name', 'last_name'):
+                if not body.get(k):
+                    return jsonify({'error': errors.FIELD_EMPTY.format(k)}), 422
+        else:
+            return jsonify({'error': errors.FIELD_INVALID.format('class')}), 422
+
 
         try:
+            connection.begin()
             with connection.cursor() as cursor:
                 cursor.execute('INSERT INTO accounts (class, email, password) VALUES (%(class)s, %(email)s, %(password)s)', body)
+                body['account_id'] = cursor.lastrowid
+
+                if body['class'] == 'student':
+                    cursor.execute('INSERT INTO students (account_id, university_number) VALUES (%(account_id)s, %(university_number)s)', body)
+                else:
+                    cursor.execute('INSERT INTO professors (account_id, first_name, last_name) VALUES (%(account_id)s, %(first_name)s, %(last_name)s)', body)
 
             connection.commit()
 
-            return None, 201
+            return jsonify(None), 201
         except pymysql.err.IntegrityError as e:
+            connection.rollback()
             logger.error(e)
-            return jsonify({'error': errors.DATA_SAVE}), 403
+            return jsonify({'error': errors.DATA_SAVE}), 500
+
 
 class AccountAuthenticationView(MethodView):
     def get(self):
         try:
             check(request.headers.get('Authorization'))
         except errors.AuthenticationError():
-            return jsonify({'error': AUTHENTICATION_INVALID}), 401
+            return jsonify({'error': errors.AUTHENTICATION_INVALID}), 401
 
         return jsonify({'data': 'okay'}), 200
 
@@ -51,7 +71,7 @@ class AccountAuthenticationView(MethodView):
         body = request.get_json()
 
         if not body:
-            return jsonify({'error': DATA_EMPTY}), 403
+            return jsonify({'error': errors.DATA_EMPTY}), 422
 
         try:
             with connection.cursor() as cursor:
@@ -61,7 +81,7 @@ class AccountAuthenticationView(MethodView):
                 logger.debug(account)
 
                 if account is None or not password.check(body['password'], account['password']):
-                    return jsonify({'error': errors.AUTHENTICATION_WRONG}), 403
+                    return jsonify({'error': errors.AUTHENTICATION_WRONG}), 422
 
                 if account['class'] == 'student':
                     cursor.execute('SELECT * FROM students WHERE account_id=%(id)s', account)
@@ -79,4 +99,4 @@ class AccountAuthenticationView(MethodView):
                 return jsonify({'data': auth.generate(account)}), 200
         except pymysql.err.IntegrityError as e:
             logger.error(e)
-            return jsonify({'error': errors.DATA_SAVE}), 403
+            return jsonify({'error': errors.DATA_SAVE}), 500
