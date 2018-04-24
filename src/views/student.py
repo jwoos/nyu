f492d865d93cff8cd677f76e1727402c2ce07e9f
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-24 10:07:42 -0400

    student query params

--- src/views/student.py
+++ src/views/student.py
@@ -20,27 +20,67 @@ class StudentView(MethodView):
 
 class StudentEnrollmentView(MethodView):
     def get(self, student_id):
+        year = request.args.get('year')
+        semester = request.args.get('semester')
+
         with connection.cursor() as cursor:
-            # all
-            cursor.execute(
-                'SELECT * FROM enrollments WHERE student_id=%(student_id)s',
-                {'student_id': student_id}
-            )
+            if year and semester:
+                # by year and semester
+                cursor.execute(
+                    'SELECT * FROM enrollments WHERE student_id=%(student_id)s AND year=%(year)s AND semester=%(semester)s',
+                    {'student_id': student_id, 'year': year, 'semester': semester}
+                )
+            elif year:
+                # by year
+                cursor.execute(
+                    'SELECT * FROM enrollments WHERE student_id=%(student_id)s AND year=%(year)s',
+                    {'student_id': student_id, 'year': year}
+                )
+            else:
+                # all
+                cursor.execute(
+                    'SELECT * FROM enrollments WHERE student_id=%(student_id)s',
+                    {'student_id': student_id}
+                )
 
             return jsonify(cursor.fetchall()), 200
 
 
 class StudentEvaluationEview(MethodView):
     def get(self, student_id, evaluation_id=None):
+        year = request.args.get('year')
+        semester = request.args.get('semester')
+
         with connection.cursor() as cursor:
-            # all
-            cursor.execute(
-                '''
-                SELECT * FROM evaluations WHERE enrollment_id IN (
-                    SELECT id FROM enrollment WHERE student_id=%(student_id)s
+            if year and semester:
+                # by year and semester
+                cursor.execute(
+                    '''
+                    SELECT * FROM evaluations WHERE enrollment_id IN (
+                        SELECT * FROM enrollments WHERE student_id=%(student_id)s AND year=%(year)s AND semester=%(semester)s
+                    )
+                    ''',
+                    {'student_id': student_id, 'year': year, 'semester': semester}
                 )
-                ''',
-                {'student_id': student_id}
-            )
+            elif year:
+                # by year
+                cursor.execute(
+                    '''
+                    SELECT * FROM evaluations WHERE enrollment_id IN (
+                        SELECT * FROM enrollments WHERE student_id=%(student_id)s AND year=%(year)s
+                    )
+                    ''',
+                    {'student_id': student_id, 'year': year}
+                )
+            else:
+                # all
+                cursor.execute(
+                    '''
+                    SELECT * FROM evaluations WHERE enrollment_id IN (
+                        SELECT id FROM enrollment WHERE student_id=%(student_id)s
+                    )
+                    ''',
+                    {'student_id': student_id}
+                )
 
             return jsonify(cursor.fetchall()), 200
