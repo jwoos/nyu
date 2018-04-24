f62030927f6621d97ab5da696297db80eafeb607
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-24 00:56:49 -0400

    register done

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -17,7 +17,7 @@ window.addEventListener('load', () => {
 			});
 
 			let data = await response.json();
-			console.log(data);
+
 			window.common.setAccount(data.data.account);
 			window.common.setToken(data.data.token);
 
@@ -27,11 +27,30 @@ window.addEventListener('load', () => {
 		}
 	});
 
+	document.querySelector('#register-class').addEventListener('change', () => {
+		const type = document.querySelector('#register-class').value;
+		document.querySelector(`#register-${type}-section`).className = '';
+
+		if (type === 'student') {
+			document.querySelector('#register-professor-section').className = 'd-none';
+		} else {
+			document.querySelector('#register-student-section').className = 'd-none';
+		}
+	});
+
 	document.querySelector('#register-submit').addEventListener('click', async () => {
 		let body = {
-			email: document.querySelector('#register-email').value(),
-			password: document.querySelector('#register-password').value()
+			email: document.querySelector('#register-email').value,
+			password: document.querySelector('#register-password').value,
+			class: document.querySelector('#register-class').value
 		};
+
+		if (body.class === 'student') {
+			body['university_number'] = document.querySelector('#register-student-unumber').value;
+		} else {
+			body['first_name'] = document.querySelector('#register-professor-fname').value;
+			body['last_name'] = document.querySelector('#register-professor-lname').value;
+		}
 
 		try {
 			let response = await fetch(`${BASE}/accounts`, {
@@ -39,6 +58,8 @@ window.addEventListener('load', () => {
 				headers: {'Content-Type': 'application/json'},
 				body: JSON.stringify(body)
 			});
+
+			window.location.replace('/');
 		} catch (e) {
 			console.error(e);
 		}
