12ba870298a30704f9821502cb012f8433974841
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-04-25 00:33:44 -0400

    professor data

--- static/assets/scripts/professor.js
+++ static/assets/scripts/professor.js
@@ -63,34 +63,72 @@ window.professor = {
 		const aggregated = {};
 
 		for (const val of data.data) {
-			const key = `${val.name} (${val.semester}-${val.year})`;
+			const key = `${val.name}`;
 			if (!(key in aggregated)) {
-				aggregated[key] = [];
+				aggregated[key] = {};
 			}
 
-			aggregated[key].push(val);
+			const subkey = `${val.semester}-${val.year}`;
+			if (!(subkey in aggregated[key])) {
+				aggregated[key][subkey] = [];
+			}
+
+			aggregated[key][subkey].push(val);
 		}
 
-		for (const k in aggregated) {
-			const enrollment = aggregated[k];
-			const enrollmentData = {
-				ratingCount: 0,
-				ratingTotal: 0,
-				comments: []
-			};
+		for (const key in aggregated) {
+			const semesterYear = aggregated[key];
+			const semesterYearData = {};
 
-			for (const v of enrollment) {
-				enrollmentData.ratingCount++;
-				enrollmentData.ratingTotal += v.rating;
-				enrollmentData.comments.push(v.comments);
+			for (const subkey in semesterYear) {
+				if (!(subkey in semesterYearData)) {
+					semesterYearData[subkey] = {
+						ratingCount: 0,
+						ratingTotal: 0,
+						comments: []
+					};
+				}
+
+				for (const d of semesterYear[subkey]) {
+					semesterYearData[subkey].ratingCount++;
+					semesterYearData[subkey].ratingTotal += d.rating;
+					semesterYearData[subkey].comments.push(d.comments);
+				}
 			}
+
+			console.log(semesterYearData);
+
+			const canvas = document.createElement('canvas');
+			const chart = new Chart(canvas.getContext('2d'), {
+				type: 'line',
+				data: {
+					labels: Object.keys(semesterYearData),
+					datasets: [
+						{
+							label: 'Average Rating',
+							data: Object.values(semesterYearData).map(x => x.ratingTotal / x.ratingCount),
+							fill: false,
+							borderColor: randomColor({luminosity: 'light', hue: 'random', format: 'rgb'}),
+							lineTension: 0.1
+						},
+						{
+							label: 'Total Rating',
+							data: Object.values(semesterYearData).map(x => x.ratingCount),
+							fill: false,
+							borderColor: randomColor({luminosity: 'dark', hue: 'random', format: 'rgb'}),
+							lineTension: 0.1
+						}
+					]
+				},
+				options:{}
+			});
 
 			const card = document.createElement('div');
 			card.className = 'card';
 
 			const header = document.createElement('div');
 			header.className = 'card-header';
-			header.innerText = k;
+			header.innerText = key;
 
 			const body = document.createElement('div');
 			body.className = 'card-body';
@@ -98,8 +136,9 @@ window.professor = {
 			const text = document.createElement('div');
 			text.className = 'card-text';
 
-			text.innerText = `Total ratings: ${enrollmentData.ratingCount}\nAverage Rating: ${enrollmentData.ratingTotal / enrollmentData.ratingCount}\nComments: ${enrollmentData.comments}`;
+			text.innerText = `Comments:\n${Object.values(semesterYearData).map(x => x.comments.join('\n')).join('\n')}`;
 
+			body.appendChild(canvas);
 			body.appendChild(text);
 			card.appendChild(header);
 			card.appendChild(body);
