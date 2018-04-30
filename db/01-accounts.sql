601d5e0fe35bdd04f9e6d72a4ab88575f10e2073
YinWang3026 <yw2178@nyu.edu>
2018-04-29 21:33:05 -0400

    Revert "Combine log out and mock"
    This reverts commit 1093508ae2a641ed3d94845a147775333a6a943d.

--- db/01-accounts.sql
+++ db/01-accounts.sql
@@ -7,55 +7,7 @@ INSERT INTO accounts (
 ) VALUES (
 	1,
 	"student",
-	"student@test.site1",
-	"$2b$12$Ejcx.43PI8XcLVzTQPPxBeSxUfabYiYs2dQUhupjTm6iMk6sReWBe"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	2,
-	"student",
-	"student@test.site2",
-	"$2b$12$Ejcx.43PI8XcLVzTQPPxBeSxUfabYiYs2dQUhupjTm6iMk6sReWBe"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	3,
-	"student",
-	"student@test.site3",
-	"$2b$12$Ejcx.43PI8XcLVzTQPPxBeSxUfabYiYs2dQUhupjTm6iMk6sReWBe"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	4,
-	"student",
-	"student@test.site4",
-	"$2b$12$Ejcx.43PI8XcLVzTQPPxBeSxUfabYiYs2dQUhupjTm6iMk6sReWBe"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	5,
-	"student",
-	"student@test.site5",
+	"student@test.site",
 	"$2b$12$Ejcx.43PI8XcLVzTQPPxBeSxUfabYiYs2dQUhupjTm6iMk6sReWBe"
 );
 
@@ -68,54 +20,6 @@ INSERT INTO accounts (
 ) VALUES (
 	2,
 	"professor",
-	"professor@test.site1",
-	"$2b$12$SQ3hxOnjj29/RM2Ua3CsKuYMmNpzUZo0ugw/oIYPqWIx7E8HVg0BC"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	3,
-	"professor",
-	"professor@test.site2",
-	"$2b$12$SQ3hxOnjj29/RM2Ua3CsKuYMmNpzUZo0ugw/oIYPqWIx7E8HVg0BC"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	4,
-	"professor",
-	"professor@test.site3",
-	"$2b$12$SQ3hxOnjj29/RM2Ua3CsKuYMmNpzUZo0ugw/oIYPqWIx7E8HVg0BC"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	5,
-	"professor",
-	"professor@test.site4",
-	"$2b$12$SQ3hxOnjj29/RM2Ua3CsKuYMmNpzUZo0ugw/oIYPqWIx7E8HVg0BC"
-);
-
-INSERT INTO accounts (
-	id,
-	class,
-	email,
-	password
-) VALUES (
-	6,
-	"professor",
-	"professor@test.site5",
+	"professor@test.site",
 	"$2b$12$SQ3hxOnjj29/RM2Ua3CsKuYMmNpzUZo0ugw/oIYPqWIx7E8HVg0BC"
 );
