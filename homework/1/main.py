3d64643c6c4a2f5578f1451f92ff41700740e95e
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-09-22 16:01:58 +0000

    incoming/outgoing works

--- homework/1/main.py
+++ homework/1/main.py
@@ -9,7 +9,20 @@ BUFFER_SIZE = 1024
 def main(sock):
     while True:
         conn, addr = sock.accept()
-        k
+        message = []
+
+        while True:
+            received = conn.recv(BUFFER_SIZE)
+            message.append(received)
+
+            if len(received) < BUFFER_SIZE:
+                break
+
+        raw_message = b''.join(message)
+        print(raw_message)
+        # TODO parse http here
+
+        conn.send(b'HTTP/1.1 200 OK\r\nServer: js8460\r\nContent-Length: 4\r\nContent-type: text/html\r\nConnection: Closed\r\n\r\ntest')
 
 
 if __name__ == '__main__':
@@ -18,7 +31,8 @@ if __name__ == '__main__':
             family=socket.AF_INET,
             type=socket.SOCK_STREAM,
         )
-        sock.bind(('127.0.0.1', 8080))
+        # sock.bind(('127.0.0.1', 8080))
+        sock.bind(('0.0.0.0', 8080))
         sock.listen()
 
         main(sock)
